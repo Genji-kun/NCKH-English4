8d420562d6c5aedcbb271d09f753d48b714079fa
Genji-kun <2051052097phat@ou.edu.vn>
2023-12-12 17:07:50 +0700

    [Phat] - Update pdate Project

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </div>
+  )
+}
---
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { LucideIcon } from 'lucide-react';
+import React from 'react';
+
+interface SidebarItemProps {
+    icon: LucideIcon,
+    label: string,
+    href: string
+};
+
+const SidebarItem = ({ icon, label, href }: SidebarItemProps) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SidebarItem;
